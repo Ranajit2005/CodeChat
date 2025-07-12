523d5401b09a503994b888ca827672000e76e286
Ranajit2005 <18ranajit2005@gmail.com>
2025-07-12 21:16:05 +0530

    update

--- frontend/src/components/MessageArea.jsx
+++ frontend/src/components/MessageArea.jsx
@@ -1,4 +1,9 @@
+import { useSelector } from "react-redux";
+
+
 const MessageArea = () => {
+  const { userData } = useSelector((state) => state.user);
+  console.log("userData in MessageArea:", userData);
   return (
     <div className="lg:w-2/3 w-full h-full hidden sm:block overflow-hidden relative">
 
@@ -10,9 +15,16 @@ const MessageArea = () => {
 
       {/* Overlay, for absolute */}
       <div className="absolute w-full z-10 h-full flex flex-col">
+
+        
         {/* Header */}
-        <div className="bg-gradient-to-br from-purple-500 to-[#20c7ff] py-3 rounded-b-[30px] flex items-center justify-start gap-3 text-black pl-3">
-          <h1 className="text-xl">CodeChat</h1>
+        <div className="bg-gradient-to-br from-purple-500 to-[#20c7ff] py-3 rounded-b-[30px] flex items-center justify-start gap-3 text-white pl-3">
+          <img
+            src={userData?.image || "/defaultProfile.png"}
+            className="h-8 w-8 sm:h-10 sm:w-10 rounded-full"
+            alt="Chat Icon"
+          />
+          <h1 className="text-xl">{userData.username}</h1>
         </div>
 
       
