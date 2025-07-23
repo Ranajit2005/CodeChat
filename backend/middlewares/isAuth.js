523b6cd2d82fb7509dca3e3db5cb60484601416d
Ranajit2005 <18ranajit2005@gmail.com>
2025-07-24 01:26:56 +0530

    Fix typo in unauthorized response JSON in authentication middleware

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -5,7 +5,7 @@ const isAuth = async (req,res,next) =>{
         const token = req.cookies.token;
 
         if(!token){
-            return res.status(401).josn({
+            return res.status(401).json({
                 success: false,
                 message: "Unauthorized, please login"
             })
